51d48568f07972ce09c063670ac7882bb1520c0a
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-08-04 11:36:50 +0200

    update version to v0.5

--- project/Commons.scala
+++ project/Commons.scala
@@ -4,7 +4,7 @@ import Keys._
 object Commons {
   val commonSettings = Seq(
     organization := "de.upb.cs.uc4",
-    version := "v0.4.4",
+    version := "v0.5",
     scalaVersion := "2.13.0"
   )
 }
