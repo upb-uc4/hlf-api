bb553038b1eda6a3e613c890e33431cf79582ee0
NikoB <niko.bergemann@web.de>
2020-08-11 10:38:40 +0200

    fix and reenable connectionTests

--- src/test/scala/ConnectionTests.scala
+++ src/test/scala/ConnectionTests.scala
@@ -1,5 +1,7 @@
 import java.nio.file.{Path, Paths}
 
+import de.upb.cs.uc4.hyperledger.ConnectionManager
+import de.upb.cs.uc4.hyperledger.utilities.{GatewayManager, WalletManager}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -7,65 +9,104 @@ class ConnectionTests extends AnyWordSpec with Matchers {
 
   val connection_profile_path: Path = Paths.get(getClass.getResource("/connection_profile.yaml").toURI)
   val wallet_path: Path = Paths.get(getClass.getResource("/wallet/").toURI)
+  val contained_id: String = "cli"
+  val channel_name: String = "myc"
+  val chaincode: String = "mycc"
+  val contract_name_course = "UC4.course"
 
-  /*
-  val connectionManager = ConnectionManager(
-    Paths.get(getClass.getResource("/connection_profile.yaml").toURI),
-    Paths.get(getClass.getResource("/wallet/").toURI))
+  val connectionManager: ConnectionManager = ConnectionManager(connection_profile_path, wallet_path)
 
-  "The connectionManager" when {
-    "connecting to Chain" should {
-
-      "provide gateway connection" in {
+  "The WalletManager" when {
+    "asked for a wallet" should {
+      "not return null" in {
         // retrieve possible identities
         val wallet = WalletManager.getWallet(wallet_path)
         wallet should not be null
-
-        // prepare Network Builder
-        val builder = connectionManager.getBuilder(wallet)
-        builder should not be null
-
-        // get gateway object
-        val gateway = builder.connect
-        gateway should not be null
-
-        // cleanup
-        connectionManager.disposeGateway(gateway)
       }
-
-      "provide network connection" in {
+      "contain cli" in {
         // retrieve possible identities
-        val wallet = connectionManager.getWallet()
-        wallet should not be null
-
-        // prepare Network Builder
-        val builder = connectionManager.getBuilder(wallet)
-        builder should not be null
-
-        // get gateway object
-        val gateway = builder.connect
-        gateway should not be null
-
-        // try connecting to the network
-        try {
-          val network = gateway.getNetwork(connectionManager.channel_name)
-          network should not be null
-        } finally {
-          connectionManager.disposeGateway(gateway)
-        }
-      }
-
-      "Provice chaincode connection" in {
-        // test full chaincode connection
-        val (gateway, contract_course, contract_student) = connectionManager.initializeConnection()
-        gateway should not be null
-        contract_course should not be null
-        contract_student should not be null
-
-        // cleanup
-        connectionManager.disposeGateway(gateway)
+        val wallet = WalletManager.getWallet(wallet_path)
+        wallet.list() should contain this.contained_id
       }
     }
   }
-   */
+
+  "The GatewayManager" when {
+    "asked to setup a gateway" should {
+      "provide a builder" in {
+        // retrieve possible identities
+        val wallet = WalletManager.getWallet(wallet_path)
+
+        // prepare Network Builder
+        val builder = GatewayManager.getBuilder(wallet, connection_profile_path, contained_id)
+        builder should not be null
+      }
+
+      "provide a gateway" in {
+        // retrieve possible identities
+        val wallet = WalletManager.getWallet(wallet_path)
+
+        // prepare Network Builder
+        val builder = GatewayManager.getBuilder(wallet, connection_profile_path, contained_id)
+        builder should not be null
+
+        // get gateway object
+        val gateway = GatewayManager.createGateway(wallet, connection_profile_path, contained_id)
+        gateway should not be null
+
+        // cleanup
+        GatewayManager.disposeGateway(gateway)
+      }
+      "provide a gateway pointing to our network channel" in {
+        // retrieve possible identities
+        val wallet = WalletManager.getWallet(wallet_path)
+
+        // prepare Network Builder
+        val builder = GatewayManager.getBuilder(wallet, connection_profile_path, contained_id)
+        builder should not be null
+
+        // get gateway object
+        val gateway = GatewayManager.createGateway(wallet, connection_profile_path, contained_id)
+        gateway should not be null
+
+        try {
+          val network = gateway.getNetwork(channel_name)
+          network should not be null
+        } finally {
+          // cleanup
+          GatewayManager.disposeGateway(gateway)
+        }
+      }
+      "provide a gateway pointing to our network channel containing our course_contract" in {
+        // retrieve possible identities
+        val wallet = WalletManager.getWallet(wallet_path)
+
+        // prepare Network Builder
+        val builder = GatewayManager.getBuilder(wallet, connection_profile_path, contained_id)
+        builder should not be null
+
+        // get gateway object
+        val gateway = GatewayManager.createGateway(wallet, connection_profile_path, contained_id)
+        gateway should not be null
+
+        try {
+          val network = gateway.getNetwork(channel_name)
+          val contract = network.getContract(chaincode, contract_name_course)
+          contract should not be null
+        } finally {
+          // cleanup
+          GatewayManager.disposeGateway(gateway)
+        }
+      }
+    }
+  }
+
+  "The Connection Manager" when {
+    "asked for a connection" should {
+      "provide network connection" in {
+        val connection = connectionManager.createConnection()
+        connection should not be null
+      }
+    }
+  }
 }
