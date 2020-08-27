663f4f0853f0cd5e322b04a393f817feb0716e4a
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-08-27 15:39:13 +0200

    Add error tests for AddEntry

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/MatriculationErrorTests.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/MatriculationErrorTests.scala
@@ -12,6 +12,7 @@ class MatriculationErrorTests extends TestBase {
   override def beforeAll(): Unit = {
     super.beforeAll()
     chaincodeConnection = initializeMatriculation()
+    chaincodeConnection.addMatriculationData(TestDataMatriculation.validMatriculationData1("001"))
   }
 
   override def afterAll(): Unit = {
@@ -25,7 +26,11 @@ class MatriculationErrorTests extends TestBase {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.getMatriculationData("110"))
         result.transactionId should ===("getMatriculationData")
-        println(result.payload)
+      }
+      "throw TransactionException for empty matriculationId-String " in {
+        // test action
+        val result = intercept[TransactionException](() -> chaincodeConnection.getMatriculationData(""))
+        result.transactionId should ===("getMatriculationData")
       }
     }
 
@@ -34,91 +39,121 @@ class MatriculationErrorTests extends TestBase {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonNoSemester("120")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (missing Field of Study) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonNoFieldOfStudy("121")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (missing matriculationStatus) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonNoMatriculationStatus("122")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (missing MatId) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonNoMatriculationId("123")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (missing FirstName) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonNoFirstName("124")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (missing LastName) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonNoLastName("125")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (missing Birthdate) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonNoBirthdate("126")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (invalid Birthdate) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonInvalidBirthdate("130")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (invalid id) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonInvalidId))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (invalid data 1) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonInvalidMatriculationData1("131")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (invalid data 2) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonInvalidMatriculationData2("132")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (invalid data 3) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonInvalidMatriculationData3("133")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
       }
 
       "throw TransactionException for malformed json Input (invalid data 4) " in {
         // test action
         val result = intercept[TransactionException](() -> chaincodeConnection.addMatriculationData(TestDataMatriculation.invalidMatriculationJsonInvalidMatriculationData4("134")))
         result.transactionId should ===("addMatriculationData")
-        println(result.payload)
+      }
+    }
+
+    "invoking addMatriculationData" should {
+      "throw TransactionException for not existing matriculationId " in {
+        // test action
+        val id="140"
+        val fieldOfStudy="ComputerScience"
+        val semester="SS2020"
+        val result = intercept[TransactionException](() -> chaincodeConnection.addEntryToMatriculationData(id, fieldOfStudy, semester))
+        result.transactionId should ===("addEntryToMatriculationData")
+      }
+      "throw TransactionException for empty matriculationId " in {
+        // test action
+        val id=""
+        val fieldOfStudy="ComputerScience"
+        val semester="SS2020"
+        val result = intercept[TransactionException](() -> chaincodeConnection.addEntryToMatriculationData(id, fieldOfStudy, semester))
+        result.transactionId should ===("addEntryToMatriculationData")
+      }
+      "throw TransactionException for malformed semester Entry " in {
+        // test action
+        val id="001"
+        val fieldOfStudy="ComputerScience"
+        val semester="S2020"
+        val result = intercept[TransactionException](() -> chaincodeConnection.addEntryToMatriculationData(id, fieldOfStudy, semester))
+        result.transactionId should ===("addEntryToMatriculationData")
+      }
+      "throw TransactionException for empty semester Entry " in {
+        // test action
+        val id="001"
+        val fieldOfStudy="ComputerScience"
+        val semester=""
+        val result = intercept[TransactionException](() -> chaincodeConnection.addEntryToMatriculationData(id, fieldOfStudy, semester))
+        result.transactionId should ===("addEntryToMatriculationData")
+      }
+      "throw TransactionException for empty fieldOfStudy Entry " in {
+        // test action
+        val id="001"
+        val fieldOfStudy=""
+        val semester="SS2020"
+        val result = intercept[TransactionException](() -> chaincodeConnection.addEntryToMatriculationData(id, fieldOfStudy, semester))
+        result.transactionId should ===("addEntryToMatriculationData")
       }
     }
   }
