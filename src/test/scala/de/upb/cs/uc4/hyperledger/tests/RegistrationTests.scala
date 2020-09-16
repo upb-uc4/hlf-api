84ced973f043511cc56eb6f24ad3c681a9a284ac
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-09-16 17:26:23 +0200

    fix course tests

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/RegistrationTests.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/RegistrationTests.scala
@@ -27,8 +27,8 @@ class RegistrationTests extends TestBase {
 
         println("Access Chain as TestUser")
         val connection = initializeCourses(testUserName)
-        connection.addCourse("{\"courseId\":\"" + "1" + "\",\"courseName\":\"IQC\",\"courseType\":\"Lecture\",\"startDate\":\"1998-01-01\",\"endDate\":\"1999-01-01\",\"ects\":7,\"lecturerId\":\"Mustermann\",\"maxParticipants\":80,\"currentParticipants\":20,\"courseLanguage\":\"English\",\"courseDescription\":\"Fun new course\"}")
-        val result = connection.getCourseById("1")
+        connection.addCourse("{\"courseId\":\"" + "2" + "\",\"courseName\":\"IQC\",\"courseType\":\"Lecture\",\"startDate\":\"1998-01-01\",\"endDate\":\"1999-01-01\",\"ects\":7,\"lecturerId\":\"Mustermann\",\"maxParticipants\":80,\"currentParticipants\":20,\"courseLanguage\":\"English\",\"courseDescription\":\"Fun new course\"}")
+        val result = connection.getCourseById("2")
         println(result)
       }
     }
