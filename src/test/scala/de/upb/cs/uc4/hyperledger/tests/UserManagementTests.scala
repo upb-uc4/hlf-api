c1ef1f313b2e52a5a89611f3476b653bcd35b65b
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-09-22 14:29:01 +0200

    code style

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/UserManagementTests.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/UserManagementTests.scala
@@ -1,6 +1,5 @@
 package de.upb.cs.uc4.hyperledger.tests
 
-
 import de.upb.cs.uc4.hyperledger.testBase.TestBase
 import de.upb.cs.uc4.hyperledger.utilities.helper.Logger
 import de.upb.cs.uc4.hyperledger.utilities.{ EnrollmentManager, RegistrationManager, WalletManager }
