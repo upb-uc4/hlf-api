d0a89347f6de253e033984ada87caa1c860cc684
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-10-02 16:10:07 +0200

    code style

--- project/Commons.scala
+++ project/Commons.scala
@@ -8,9 +8,7 @@ object Commons {
     homepage := Some(url("https://uc4.cs.upb.de/")),
     scmInfo := Some(ScmInfo(url("https://github.com/upb-uc4/hlf-api"), "git@github.com:upb-uc4/hlf-api.git")),
     version := "v0.9.1",
-    developers := List(
-        Developer("UC4", "UC4", "UC4_official@web.de", url("https://github.com/upb-uc4"))
-    ),
+    developers += Developer("UC4", "UC4", "UC4_official@web.de", url("https://github.com/upb-uc4")),
     licenses := List("Apache 2" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt")),
     publishMavenStyle := true,
     crossPaths := false,
