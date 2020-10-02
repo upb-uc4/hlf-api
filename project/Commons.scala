84500c9433eac0c1bdeeaf2d79f82fdb2d3525f9
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-10-02 14:55:17 +0200

    provide additional project settings

--- project/Commons.scala
+++ project/Commons.scala
@@ -4,7 +4,37 @@ import sbt.{ TestFrameworks, Tests }
 object Commons {
   def commonSettings(project: String) = Seq(
     organization := "de.upb.cs.uc4",
-    version := "v0.6.2",
+    homepage := Some(url("https://uc4.cs.upb.de/")),
+    scmInfo := Some(ScmInfo(url("https://github.com/upb-uc4/hlf-api"), "git@github.com:upb-uc4/hlf-api.git")),
+    version := "v0.9.1",
+    developers := List(
+        Developer("UC4", "UC4", "UC4_official@web.de", url("https://github.com/upb-uc4"))
+    ),
+    licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0")),
+    publishMavenStyle := true,
+    crossPaths := false,
+
+    publishTo := Some(Opts.resolver.sonatypeStaging),
+
+    // realease with sbt-release plugin
+    import ReleaseTransformations._
+    releaseCrossBuild := true,
+    //releaseTagName := s"version-${if (releaseUseGlobalVersion.value) (version in ThisBuild).value else version.value}"
+    releaseProcess := Seq[ReleaseStep](
+        checkSnapshotDependencies,
+        inquireVersions,
+        runClean,
+        runTest,
+        setReleaseVersion,
+        commitReleaseVersion,
+        tagRelease,
+        releaseStepCommandAndRemaining("+publishSigned"),
+        setNextVersion,
+        commitNextVersion,
+        releaseStepCommand("sonatypeRelease"),
+        pushChanges
+    ),
+
     scalaVersion := "2.13.0",
     testOptions += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test_reports/" + project)
   )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,3 +2,6 @@
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.0")
 // Scala auto formatting tool
 addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.8.3")
+// Publish to sonatype - maven
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "2.3")
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.1.1")
