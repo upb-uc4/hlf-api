52393dc30daf9cf581a67091993aa3d9b484f814
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-10-13 13:20:53 +0200

    clean and try new pipeline stuff

--- build.sbt
+++ build.sbt
@@ -1,14 +1,9 @@
 lazy val hyperledger_api = (project in file("."))
   .settings(
-    Commons.commonSettings("hlf_api"),
+    Commons.projectSettings("hlf_api"),
+    Commons.gpgSettings,
+    Commons.commonSettings,
     description := "Scala API to access our UC4 contracts/chaincodes.",
-    /*publishTo := {
-      val nexus = "https://oss.sonatype.org/"
-      if (isSnapshot.value)
-        Some("snapshots" at nexus + "content/repositories/snapshots")
-      else
-        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
-    },*/
     libraryDependencies ++= Dependencies.scalaTestDependencies,
     libraryDependencies ++= Dependencies.hyperledgerDependencies,
   )
--- project/Commons.scala
+++ project/Commons.scala
@@ -4,7 +4,30 @@ import sbt.Keys.{ testOptions, _ }
 import sbt.{ Developer, TestFrameworks, Tests, url }
 
 object Commons {
-  def commonSettings(project: String) = Seq(
+  def projectSettings(project: String) = Seq(
+    // scala version
+    scalaVersion := "2.13.0",
+    // append -deprecation to the options passed to the Scala compiler
+    scalacOptions += "-deprecation",
+    // testOption for test-reports
+    testOptions += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test_reports/" + project)
+  )
+
+  def gpgSettings = Seq(
+    useGpg := false,
+    //useGpgPinentry := true,
+    pgpPassphrase := sys.env.get("PGP_PASSPHRASE").map(_.toArray),
+    usePgpKeyHex("02C7EAB5DE1AD596FE5CCB68DBBB1A432C70E654")
+  )
+
+  def commonSettings = Seq(
+    organization := "de.upb.cs.uc4",
+    organizationName := "uc4",
+    homepage := Some(url("https://uc4.cs.upb.de/")),
+    developers := List(Developer("UC4", "UC4", "UC4_official@web.de", url("https://github.com/upb-uc4"))),
+    licenses := List("Apache 2" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt")),
+    crossPaths := false,
+    pomIncludeRepository := { _ => false }
     // Info for Maven Publishing
     // ----------------------------------
     // set by plugins
@@ -14,19 +37,6 @@ object Commons {
     // publishMavenStyle := true,
     // publishArtifact in Test := false,
     // ----------------------------------
-    organization := "de.upb.cs.uc4",
-    organizationName := "uc4",
-    homepage := Some(url("https://uc4.cs.upb.de/")),
-    developers := List(Developer("UC4", "UC4", "UC4_official@web.de", url("https://github.com/upb-uc4"))),
-    licenses := List("Apache 2" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt")),
-    crossPaths := false,
-    pomIncludeRepository := { _ => false },
-    // ----------------------------------
-    // GPG Stuff
-    useGpg := false,
-    //useGpgPinentry := true,
-    pgpPassphrase := sys.env.get("PGP_PASSPHRASE").map(_.toArray),
-    usePgpKeyHex("02C7EAB5DE1AD596FE5CCB68DBBB1A432C70E654"),
     // ----------------------------------
     // Sonatype
     /*credentials += Credentials(
@@ -36,11 +46,5 @@ object Commons {
       sys.env.getOrElse("SONATYPE_PASS", "")
     ),*/
     // ----------------------------------
-    // scala version
-    scalaVersion := "2.13.0",
-    // append -deprecation to the options passed to the Scala compiler
-    scalacOptions += "-deprecation",
-    // testOption for test-reports
-    testOptions += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test_reports/" + project)
   )
 }
