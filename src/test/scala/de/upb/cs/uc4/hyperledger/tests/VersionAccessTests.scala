8720a3459840c4ce86185b866b805c7ae91d783e
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-10-20 14:48:54 +0200

    fix regex check

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/VersionAccessTests.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/VersionAccessTests.scala
@@ -22,17 +22,17 @@ class VersionAccessTests extends TestBase {
     super.afterAll()
   }
 
+  var regexVersionString: String = """v((\d)+\.){2}(\d)+"""
+
   "The ScalaAPI for Connections" when {
     "asked for chaincode version " should {
       "provide a valid endpoint [Certificate] " in {
         val version: String = certificateConnection.getVersion
-        val regex = new Regex("v((\\d)+\\.){2}(\\d)+")
-        regex.matches(version) should be
+        version should fullyMatch regex regexVersionString
       }
       "provide a valid endpoint [Certificate] " in {
         val version: String = matriculationConnection.getVersion
-        val regex = new Regex("v((\\d)+\\.){2}(\\d)+")
-        regex.matches(version) should be
+        version should fullyMatch regex regexVersionString
       }
     }
   }
