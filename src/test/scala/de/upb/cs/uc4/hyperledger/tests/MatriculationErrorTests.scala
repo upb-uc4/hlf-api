1c3905d912fc9b87b4b496c58071cb04896f6172
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-10-20 11:17:51 +0200

    fix compile err

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/MatriculationErrorTests.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/MatriculationErrorTests.scala
@@ -158,12 +158,10 @@ class MatriculationErrorTests extends TestBase {
 
       "throw TransactionException for super empty matriculationList " in {
         val id = "001"
-        val result = intercept[TransactionExceptionTrait](() -> chaincodeConnection.addEntriesToMatriculationData(
-          id,
-          "[]"
-        ))
-        result.transactionId should ===("addEntriesToMatriculationData")
-        println("[DEBUG] :: " + result.toString)
+        testTransactionException(
+          "addEntriesToMatriculationData",
+          () => chaincodeConnection.addEntriesToMatriculationData(id, "[]" )
+        )
       }
     }
   }
