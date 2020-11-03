5ac2b2a58e8d1086464465790bff77b786e0b271
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-11-03 10:50:26 +0100

    fix typo

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/UnsignedTransactionTests.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/UnsignedTransactionTests.scala
@@ -14,17 +14,17 @@ import org.hyperledger.fabric.sdk.transaction.TransactionContext
 
 class UnsignedTransactionTests extends TestBase {
 
-  var certificateConenction: ConnectionCertificateTrait = _
+  var certificateConnection: ConnectionCertificateTrait = _
   var matriculationConnection: ConnectionMatriculationTrait = _
 
   override def beforeAll(): Unit = {
     super.beforeAll()
-    certificateConenction = initializeCertificate()
+    certificateConnection = initializeCertificate()
     matriculationConnection = initializeMatriculation()
   }
 
   override def afterAll(): Unit = {
-    certificateConenction.close()
+    certificateConnection.close()
     matriculationConnection.close()
     super.afterAll()
   }
@@ -34,7 +34,7 @@ class UnsignedTransactionTests extends TestBase {
       "return an unsigned transaction" in {
         val enrollmentId = "100"
         val certificate = "Whatever"
-        val proposalBytes = certificateConenction.getProposalAddCertificate(enrollmentId, certificate)
+        val proposalBytes = certificateConnection.getProposalAddCertificate(enrollmentId, certificate)
         val proposal: Proposal = Proposal.parseFrom(proposalBytes)
         println("\n\n\n##########################\nHeader:\n##########################\n\n" + proposal.getHeader.toStringUtf8)
         println("\n\n\n##########################\nPayload:\n##########################\n\n" + proposal.getPayload.toStringUtf8)
@@ -45,16 +45,16 @@ class UnsignedTransactionTests extends TestBase {
       "submit the transaction to the ledger" in {
         val enrollmentId = "102"
         val certificate = "Whatever"
-        val proposalBytes = certificateConenction.getProposalAddCertificate(enrollmentId, certificate)
+        val proposalBytes = certificateConnection.getProposalAddCertificate(enrollmentId, certificate)
         val proposal: Proposal = Proposal.parseFrom(proposalBytes)
         println("\n\n\n##########################\nPROPOSALBYTES:\n##########################\n\n" + proposal.toByteString.toStringUtf8)
         println("\n\n\n##########################\nHeader:\n##########################\n\n" + proposal.getHeader.toStringUtf8)
         println("\n\n\n##########################\nPayload:\n##########################\n\n" + proposal.getPayload.toStringUtf8)
-        val transactionContext: TransactionContext = certificateConenction.contract.getNetwork.getChannel.newTransactionContext()
+        val transactionContext: TransactionContext = certificateConnection.contract.getNetwork.getChannel.newTransactionContext()
         val signature = transactionContext.signByteString(proposalBytes)
         val b64Sig = ByteString.copyFrom(Base64.getEncoder.encode(signature.toByteArray)).toStringUtf8
         println("\n\n\n##########################\nSignature:\n##########################\n\n" + b64Sig)
-        val result = certificateConenction.submitSignedProposal(proposalBytes, signature)
+        val result = certificateConnection.submitSignedProposal(proposalBytes, signature)
         println("\n\n\n##########################\nResult:\n##########################\n\n" + result)
       }
     }
@@ -65,12 +65,12 @@ class UnsignedTransactionTests extends TestBase {
         val enrollmentId = "101"
         val wrongCertificate =
           "-----BEGIN CERTIFICATE-----\nMIICxjCCAm2gAwIBAgIUGJFrzMxyOAdnJErfr+UfDrLDJb4wCgYIKoZIzj0EAwIw\nYDELMAkGA1UEBhMCVVMxFzAVBgNVBAgTDk5vcnRoIENhcm9saW5hMRQwEgYDVQQK\nEwtIeXBlcmxlZGdlcjEPMA0GA1UECxMGRmFicmljMREwDwYDVQQDEwhyY2Etb3Jn\nMTAeFw0yMDEwMjAxMDEzMDBaFw0yMTEwMjAxMDE4MDBaMDgxDjAMBgNVBAsTBWFk\nbWluMSYwJAYDVQQDEx1zY2FsYS1yZWdpc3RyYXRpb24tYWRtaW4tb3JnMTBZMBMG\nByqGSM49AgEGCCqGSM49AwEHA0IABLStxuihhyb2XU0wzMhV3Su2Dr7LUI4z/IeL\nzeUDzhcqnZxLDN5w43rV0FXu4yRq0krOaxRhpAY65dmQQ6PRrzujggErMIIBJzAO\nBgNVHQ8BAf8EBAMCA6gwHQYDVR0lBBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMAwG\nA1UdEwEB/wQCMAAwHQYDVR0OBBYEFLAa99vOXhJylch+MQGthFCG/v+RMB8GA1Ud\nIwQYMBaAFBJ7z3hS1NU4HpEaFgyWKir699s5MCgGA1UdEQQhMB+CHXNjYWxhLXJl\nZ2lzdHJhdGlvbi1hZG1pbi1vcmcxMH4GCCoDBAUGBwgBBHJ7ImF0dHJzIjp7ImFk\nbWluIjoidHJ1ZSIsImhmLkFmZmlsaWF0aW9uIjoiIiwiaGYuRW5yb2xsbWVudElE\nIjoic2NhbGEtcmVnaXN0cmF0aW9uLWFkbWluLW9yZzEiLCJoZi5UeXBlIjoiYWRt\naW4ifX0wCgYIKoZIzj0EAwIDRwAwRAIgEjWf7bQyGkHf2bj16MyQ874wCWOb8l2M\n60MlJ4eDgosCIEbD4+stNqZKKsJ+C48IerpOJD3jwkLG+8y7YuxTpx8Z\n-----END CERTIFICATE-----\n"
-        val proposalBytes = certificateConenction.getProposalAddCertificate(enrollmentId, wrongCertificate)
+        val proposalBytes = certificateConnection.getProposalAddCertificate(enrollmentId, wrongCertificate)
         val proposal: Proposal = Proposal.parseFrom(proposalBytes)
         println("\n\n\n##########################\nHeader:\n##########################\n\n" + proposal.getHeader.toStringUtf8)
         println("\n\n\n##########################\nPayload:\n##########################\n\n" + proposal.getPayload.toStringUtf8)
         val signature = ByteString.copyFrom(Base64.getDecoder.decode("MEUCIQD92OsJsVVFqFfifMV14ROiL5Ni/RaOBkR0DqzetvPfkQIgcrgu9vxr5TuZY6lft5adCETaC3CSE8QA+bs9MheeLcI="))
-        val result = intercept[HyperledgerExceptionTrait](certificateConenction.submitSignedProposal(proposalBytes, signature))
+        val result = intercept[HyperledgerExceptionTrait](certificateConnection.submitSignedProposal(proposalBytes, signature))
         result.actionName should be(approvalTransactionName)
       }
     }
