63cace1958d61cd88b3bf532b0aa9c2bb66f740f
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-11-17 13:02:45 +0100

    Delete setup_node_ip.sh
    remove obsolete file

--- setup_node_ip.sh
+++
@@ -1,5 +0,0 @@
-pushd src/test/resources
-envsubst '${NODE_IP}' < connection_profile_kubernetes_template.yaml > connection_profile_kubernetes.yaml
-popd
-
-echo "Applied cluster ip ${NODE_IP} to template."
