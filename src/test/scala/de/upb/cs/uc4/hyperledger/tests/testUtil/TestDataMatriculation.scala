f8cccbdc6bdc503fa2860f6fb2c921fe8824901f
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-11-24 13:21:51 +0100

    fix duplicate module IDs

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/testUtil/TestDataMatriculation.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/testUtil/TestDataMatriculation.scala
@@ -40,7 +40,7 @@ object TestDataMatriculation {
   def establishExaminationRegulation(connection: ConnectionExaminationRegulationTrait, name: String): Unit = {
     val existingValue = connection.getExaminationRegulations(TestHelper.getJsonList(Array("\"" + name + "\"")))
     if (existingValue == "[]") {
-      val examinationRegulation = TestDataExaminationRegulation.validExaminationRegulation(name, Array(testModule("M.1"), testModule("M.2")), state = true)
+      val examinationRegulation = TestDataExaminationRegulation.validExaminationRegulation(name, Array(testModule("MatriculationTestModule.1"), testModule("MatriculationTestModule.2")), state = true)
       connection.addExaminationRegulation(examinationRegulation)
     }
   }
