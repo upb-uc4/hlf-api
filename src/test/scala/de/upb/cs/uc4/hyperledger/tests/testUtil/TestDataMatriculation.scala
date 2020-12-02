eb1ad2cdc49cb2587cae37102aa021db2b3cc6af
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-12-02 11:27:40 +0100

    enhance existing TestData

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/testUtil/TestDataMatriculation.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/testUtil/TestDataMatriculation.scala
@@ -8,6 +8,8 @@ object TestDataMatriculation {
   def validMatriculationData2(id: String): String = "{\n  \"enrollmentId\": \"" + id + "\",\n  \"matriculationStatus\": [\n    {\n      \"fieldOfStudy\": \"Computer Science\",\n      \"semesters\": [\n        \"WS2019/20\"\n      ]\n    }\n  ]\n}"
   def validMatriculationData3(id: String): String = "{\"enrollmentId\":\"" + id + "\",\"matriculationStatus\":[{\"fieldOfStudy\":\"Media Sciences\",\"semesters\":[\"WS2020/21\"]},{\"fieldOfStudy\":\"Mathematics\",\"semesters\":[\"WS2020/21\",\"SS2018\"]}]}"
   def validMatriculationData4(id: String): String = "{\"enrollmentId\":\"" + id + "\",\"matriculationStatus\":[{\"fieldOfStudy\":\"Media Sciences\",\"semesters\":[\"WS2020/21\"]},{\"fieldOfStudy\":\"Mathematics\",\"semesters\":[\"WS2020/21\",\"SS2018\"]}]}"
+  def validMatriculationDataCustom(id: String, examinationRegulation: String): String =
+    "{\n  \"enrollmentId\": \"" + id + "\",\n  \"matriculationStatus\": [\n    {\n      \"fieldOfStudy\": \"" + examinationRegulation + "\",\n      \"semesters\": [\n        \"SS2020\"\n      ]\n    }\n  ]\n}"
 
   def invalidMatriculationJsonNoSemester(id: String): String = "{\n  \"matriculationId\": \"" + id + "\",\n  \"matriculationStatus\": [\n    {\n      \"fieldOfStudy\": \"Computer Science\",\n        }\n  ]\n}"
   def invalidMatriculationJsonNoFieldOfStudy(id: String): String = "{\n  \"matriculationId\": \"" + id + "\",\n  \"matriculationStatus\": [\n    {\n      \"semesters\": [\n        \"SS2020\"\n      ]\n    }\n  ]\n}"
