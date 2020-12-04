8a8733684eea1bc226f3382032b1f4f6a875e947
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-12-04 15:25:27 +0100

    fix tests

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/sequentialSuites/SequentialAdmissionTests.scala
+++
@@ -1,9 +0,0 @@
-package de.upb.cs.uc4.hyperledger.tests.sequentialSuites
-
-import de.upb.cs.uc4.hyperledger.tests._
-import org.scalatest.{ DoNotDiscover, Sequential }
-
-@DoNotDiscover
-class SequentialAdmissionTests extends Sequential(
-  new AdmissionAccessTests
-)
