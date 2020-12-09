ce65a2415fc1c69ba89a0ebd4c0fd8bb8bb26f97
anemonekk <32546860+anemonekk@users.noreply.github.com>
2020-12-09 14:05:38 +0100

    Update src/main/scala/de/upb/cs/uc4/hyperledger/connections/traits/ConnectionGroupTrait.scala
    Co-authored-by: Niko Bergemann <nikob@mail.uni-paderborn.de>

--- src/main/scala/de/upb/cs/uc4/hyperledger/connections/traits/ConnectionGroupTrait.scala
+++ src/main/scala/de/upb/cs/uc4/hyperledger/connections/traits/ConnectionGroupTrait.scala
@@ -32,7 +32,7 @@ trait ConnectionGroupTrait extends ConnectionTrait {
   def getProposalRemoveUserFromGroup(enrollmentId: String, groupId: String): Array[Byte]
 
   /** Retrieves a proposal for the designated query
-    * Also submits the "dropAdmission" query as current user (admin).
+    * Also submits an approval for the transaction as the current user (admin).
     *
     * @param enrollmentId enrollmentId to remove.
     * @throws TransactionExceptionTrait if chaincode throws an exception.
