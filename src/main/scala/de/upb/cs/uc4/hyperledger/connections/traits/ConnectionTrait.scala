fa7a6ec611ddfe4e2a6ab24c8d1d36476670722c
Niko Bergemann <nikob@mail.uni-paderborn.de>
2020-12-15 14:03:00 +0100

    test more logging

--- src/main/scala/de/upb/cs/uc4/hyperledger/connections/traits/ConnectionTrait.scala
+++ src/main/scala/de/upb/cs/uc4/hyperledger/connections/traits/ConnectionTrait.scala
@@ -95,18 +95,21 @@ trait ConnectionTrait extends AutoCloseable {
   // TODO read affiliation from certificate
   protected final def internalGetUnsignedProposal(certificate: String, affiliation: String, transactionName: String, params: String*): (String, Array[Byte]) = {
     // throw if transaction is faulty / illegal
+    Logger.info("Begin Test transaction")
     try {
       val executionTest = this.privateEvaluateTransaction(transactionName, params: _*)
       this.wrapTransactionResult(transactionName, executionTest)
     }
     catch {
-      case e: TransactionExceptionTrait =>{
+      case e: TransactionExceptionTrait => {
         if (!e.payload.contains("HLInsufficientApprovals")) {
           throw Logger.err("Error during test Execution", e)
         }
       }
+      case e: Throwable => throw Logger.err("Internal Error during test Execution", e)
     }
 
+    Logger.info("Begin Approve transaction")
     // approve transaction as ADMIN managing the current connection
     val approvalResult: String = approveTransaction(transactionName, params: _*)
 
