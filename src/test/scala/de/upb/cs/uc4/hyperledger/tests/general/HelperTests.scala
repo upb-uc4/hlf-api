651a1d3c823f41ab5dc9b4a0481a4bed40dba33e
Niko Bergemann <nikob@mail.uni-paderborn.de>
2021-01-19 20:40:37 +0100

    provide test for transactionInfoParsing

---
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/general/HelperTests.scala
@@ -0,0 +1,20 @@
+package de.upb.cs.uc4.hyperledger.tests.general
+
+import de.upb.cs.uc4.hyperledger.testBase.TestBase
+import de.upb.cs.uc4.hyperledger.utilities.helper.{ Logger, TransactionHelper }
+
+class HelperTests extends TestBase{
+
+  "The TransactionHelper" when {
+    "parsing OperationInfo" should {
+      "extract the correct information" in {
+        val operationInfo = "{\"operationId\":\"0rhY7SWcWIb-yjRYLMBzc3r2rZ-ar-n95Tbls6P3ClA=\",\"transactionInfo\":{\"contractName\":\"UC4.MatriculationData\",\"transactionName\":\"addMatriculationData\",\"parameters\":\"[\\\"{\\\\n \\\\\\\"enrollmentId\\\\\\\": \\\\\\\"frontend-signing-tester\\\\\\\",\\\\n \\\\\\\"matriculationStatus\\\\\\\": [\\\\n {\\\\n \\\\\\\"fieldOfStudy\\\\\\\": \\\\\\\"Computer Science\\\\\\\",\\\\n \\\\\\\"semesters\\\\\\\": [\\\\n \\\\\\\"SS2020\\\\\\\"\\\\n ]\\\\n }\\\\n ]\\\\n}\\\"]\"},\"initiator\":\"frontend-signing-tester\",\"initiatedTimestamp\":\"2021-01-19T19:02:02\",\"lastModifiedTimestamp\":\"2021-01-19T19:02:02\",\"state\":\"PENDING\",\"reason\":\"\",\"existingApprovals\":{\"users\":[\"test-admin\",\"frontend-signing-tester\"],\"groups\":[\"Admin\"]},\"missingApprovals\":{\"users\":[],\"groups\":[]}}"
+        val result = TransactionHelper.getTransactionInfoFromOperation(operationInfo)
+        val r = TransactionHelper.getInfoFromTransactionInfo(result)
+        r._1 should be ("UC4.MatriculationData")
+        r._2 should be ("addMatriculationData")
+        r._3.length should be (1)
+      }
+    }
+  }
+}
