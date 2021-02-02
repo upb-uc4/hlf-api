7d8b7f6a6130a0eb706af278122c0703736bfde9
Niko Bergemann <nikob@mail.uni-paderborn.de>
2021-02-02 13:47:51 +0100

    Update src/main/scala/de/upb/cs/uc4/hyperledger/connections/traits/ConnectionExamResultTrait.scala
    Co-authored-by: dhelmert <52128028+dhelmert@users.noreply.github.com>

--- src/main/scala/de/upb/cs/uc4/hyperledger/connections/traits/ConnectionExamResultTrait.scala
+++ src/main/scala/de/upb/cs/uc4/hyperledger/connections/traits/ConnectionExamResultTrait.scala
@@ -3,7 +3,7 @@ package de.upb.cs.uc4.hyperledger.connections.traits
 import de.upb.cs.uc4.hyperledger.exceptions.traits.{ HyperledgerExceptionTrait, TransactionExceptionTrait }
 
 trait ConnectionExamResultTrait extends ConnectionTrait {
-  final override val contractName: String = "UC4.Exam"
+  final override val contractName: String = "UC4.ExamResult"
 
   /** Retrieves a proposal for the designated query
     * Also submits approval for the query as current user (admin).
