72f4f835a240f29160de58dc39c905169e1a85b3
Niko Bergemann <nikob@mail.uni-paderborn.de>
2021-02-02 10:33:24 +0100

    adjust admissions contract to provide new methods (stil support old getAdmissions)

--- src/main/scala/de/upb/cs/uc4/hyperledger/connections/cases/ConnectionAdmission.scala
+++ src/main/scala/de/upb/cs/uc4/hyperledger/connections/cases/ConnectionAdmission.scala
@@ -2,7 +2,10 @@ package de.upb.cs.uc4.hyperledger.connections.cases
 
 import java.nio.file.Path
 
+import com.google.gson.Gson
 import de.upb.cs.uc4.hyperledger.connections.traits.ConnectionAdmissionTrait
+
+import scala.jdk.CollectionConverters.SeqHasAsJava
 
 protected[hyperledger] case class ConnectionAdmission(username: String, channel: String, chaincode: String, walletPath: Path, networkDescriptionPath: Path)
   extends ConnectionAdmissionTrait {
@@ -15,8 +18,19 @@ protected[hyperledger] case class ConnectionAdmission(username: String, channel:
     internalApproveAsCurrentAndGetProposalProposeTransaction(certificate, affiliation, "dropAdmission", admissionId)
   }
 
-  override def getProposalGetAdmission(certificate: String, affiliation: String = AFFILIATION, enrollmentId: String = "", courseId: String = "", moduleId: String = ""): (String, Array[Byte]) = {
+  override def getProposalGetAdmissions(certificate: String, affiliation: String = AFFILIATION, enrollmentId: String = "", courseId: String = "", moduleId: String = ""): (String, Array[Byte]) = {
     internalApproveAsCurrentAndGetProposalProposeTransaction(certificate, affiliation, "getAdmissions", enrollmentId, courseId, moduleId)
+  }
+
+  override def getProposalGetCourseAdmissions(certificate: String, affiliation: String = AFFILIATION, enrollmentId: String = "", courseId: String = "", moduleId: String = ""): (String, Array[Byte]) = {
+    internalApproveAsCurrentAndGetProposalProposeTransaction(certificate, affiliation, "getCourseAdmissions", enrollmentId, courseId, moduleId)
+  }
+
+  override def getProposalGetExamAdmissions(certificate: String, affiliation: String = AFFILIATION, admissionIds: List[String], enrollmentId: String, examIds: List[String]): (String, Array[Byte]) = {
+    internalApproveAsCurrentAndGetProposalProposeTransaction(certificate, affiliation, "getExamAdmissions",
+      new Gson().toJson(admissionIds.asJava),
+      enrollmentId,
+      new Gson().toJson(examIds.asJava))
   }
 
   override def addAdmission(admission: String): String =
@@ -27,4 +41,13 @@ protected[hyperledger] case class ConnectionAdmission(username: String, channel:
 
   override def getAdmissions(enrollmentId: String = "", courseId: String = "", moduleId: String = ""): String =
     wrapSubmitTransaction(false, "getAdmissions", enrollmentId, courseId, moduleId)
+
+  override def getCourseAdmissions(enrollmentId: String = "", courseId: String = "", moduleId: String = ""): String =
+    wrapSubmitTransaction(false, "getCourseAdmissions", enrollmentId, courseId, moduleId)
+
+  override def getExamAdmissions(admissionIds: List[String], enrollmentId: String, examIds: List[String]): String =
+    wrapSubmitTransaction(false, "getExamAdmissions",
+      new Gson().toJson(admissionIds.asJava),
+      enrollmentId,
+      new Gson().toJson(examIds.asJava))
 }
