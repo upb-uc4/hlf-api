b8b75741c382f9734a518a91628206fa3e1b8059
Niko Bergemann <nikob@mail.uni-paderborn.de>
2021-02-02 10:26:12 +0100

    Add ExamResult Connection

---
+++ src/main/scala/de/upb/cs/uc4/hyperledger/connections/cases/ConnectionExamResult.scala
@@ -0,0 +1,29 @@
+package de.upb.cs.uc4.hyperledger.connections.cases
+
+import java.nio.file.Path
+
+import com.google.gson.Gson
+import de.upb.cs.uc4.hyperledger.connections.traits.ConnectionExamResultTrait
+
+import scala.jdk.CollectionConverters.SeqHasAsJava
+
+case class ConnectionExamResult(username: String, channel: String, chaincode: String, walletPath: Path, networkDescriptionPath: Path)
+  extends ConnectionExamResultTrait {
+
+  def getProposalAddExamResult(certificate: String, affiliation: String = AFFILIATION, examResultJson: String): (String, Array[Byte]) = {
+    internalApproveAsCurrentAndGetProposalProposeTransaction(certificate, affiliation, "addExamResult", examResultJson)
+  }
+
+  def getProposalGetExamResultEntries(certificate: String, affiliation: String = AFFILIATION,
+                                      enrollmentId: String, examIds: List[String]): (String, Array[Byte]) = {
+    internalApproveAsCurrentAndGetProposalProposeTransaction(certificate, affiliation, "getExamResultEntries",
+      enrollmentId,
+      new Gson().toJson(examIds.asJava))
+  }
+
+  def addExamResult(examResultJson: String): String =
+    wrapSubmitTransaction(false, "addExamResult", examResultJson)
+
+  def getExamResultEntries(enrollmentId: String, examIds: List[String]): String =
+    wrapEvaluateTransaction("getExamResultEntries", enrollmentId, new Gson().toJson(examIds.asJava))
+}
