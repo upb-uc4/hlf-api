a32327807acfb39c04a47b2d75b9b98b24f36eb3
Niko Bergemann <nikob@mail.uni-paderborn.de>
2021-02-03 15:32:10 +0100

    additional examTests(deny)

--- src/test/scala/de/upb/cs/uc4/hyperledger/tests/contracts/ExamTests.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/tests/contracts/ExamTests.scala
@@ -5,6 +5,7 @@ import de.upb.cs.uc4.hyperledger.exceptions.traits.TransactionExceptionTrait
 import de.upb.cs.uc4.hyperledger.testBase.TestBase
 import de.upb.cs.uc4.hyperledger.testUtil._
 import de.upb.cs.uc4.hyperledger.utilities.helper.Logger
+import org.scalatest.matchers.should.Matchers.intercept
 
 class ExamTests extends TestBase {
 
@@ -45,15 +46,15 @@ class ExamTests extends TestBase {
 
   "The ScalaAPI for Exams" when {
     "invoked with addExam correctly " should {
-      val testData: Seq[(String, String, String, String, String, Int)] = Seq(
-        ("allow for adding new Exam (Approvals forged)", "C.1", testUser1, testModule1, "Written Exam", 6),
-        ("allow for adding new Exam (Approvals forged)", "C.1", testUser1, testModule1, "Written Exam", 2),
-        ("allow for adding new Exam (Approvals forged)", "C.1", testUser1, testModule1, "Written Exam", 0),
-        ("allow for adding new Exam (Approvals forged)", "C.1", testUser1, testModule1, "Oral Exam", 99),
-        ("allow for adding new Exam (Approvals forged)", "C.1", testUser1, testModule1, "Oral Exam", 0),
-        ("allow for adding new Exam (Approvals forged)", "C.1", testUser1, testModule1, "Garbage", 0)
+      val testDataAllow: Seq[(String, String, String, String, String, Int)] = Seq(
+        ("allow for adding new valid Exam (Approvals forged)", "C.1", testUser1, testModule1, "Written Exam", 6),
+        ("allow for adding new valid Exam (Approvals forged)", "C.1", testUser1, testModule1, "Written Exam", 2),
+        ("allow for adding new valid Exam (Approvals forged)", "C.1", testUser1, testModule1, "Written Exam", 0),
+        ("allow for adding new valid Exam (Approvals forged)", "C.1", testUser1, testModule1, "Oral Exam", 99),
+        ("allow for adding new valid Exam (Approvals forged)", "C.1", testUser1, testModule1, "Oral Exam", 0),
+        ("allow for adding new valid Exam (Approvals forged)", "C.1", testUser1, testModule1, "Garbage", 0)
       )
-      for ((statement: String, courseId: String, lecturerId: String, moduleId: String, examType: String, ects: Int) <- testData) {
+      for ((statement: String, courseId: String, lecturerId: String, moduleId: String, examType: String, ects: Int) <- testDataAllow) {
         s"$statement [$courseId, $lecturerId, $moduleId, $examType, $ects]" in {
           Logger.info(s"Begin test: $statement with [$courseId, $lecturerId, $moduleId, $examType, $ects]")
 
@@ -71,9 +72,74 @@ class ExamTests extends TestBase {
           TestHelperStrings.compareJson(expectedResult, testResult)
         }
       }
+
+      val testDataDeny: Seq[(String, String, String, String, String, String, Int, String, String, String)] = Seq(
+        ("deny adding invalid Exam", s"Garbage", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1+1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2+1:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1+1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam+1:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:01", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1+1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser1, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1+1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam+1", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6+1, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:01", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", "", testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, "", "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        // ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", null, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"), null case not possible
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", ""),
+        ("deny adding invalid Exam", s"C.1:Garbage:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", "Garbage", testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:Garbage:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, "Garbage", "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Garbage:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Garbage", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:0:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 0, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2020-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2020-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2020-03-10T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2020-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-15T12:00:00", "2021-03-12T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-15T12:00:00"),
+        ("deny adding invalid Exam", s"C.1:$testUser2:$testModule1:Written Exam:6:2021-03-14T12:00:00", "C.1", testUser2, testModule1, "Written Exam", 6, "2021-03-14T12:00:00", "2021-03-10T12:00:00", "2021-03-08T12:00:00"),
+      )
+      for ((statement: String, examId: String, courseId: String, lecturerId: String, moduleId: String, examType: String, ects: Int, date: String, admitUntil: String, dropUntil: String) <- testDataDeny) {
+        s"directly $statement [$examId, $courseId, $lecturerId, $moduleId, $examType, $ects, $date, $admitUntil, $dropUntil]" in {
+          Logger.info(s"Begin test: $statement with [$examId, $courseId, $lecturerId, $moduleId, $examType, $ects, $date, $admitUntil, $dropUntil]")
+
+          // prepare data
+          val testExam = TestDataExam.customizableExam(examId, courseId, lecturerId, moduleId, examType, date, ects, admitUntil, dropUntil)
+
+          // implicit approval
+          val result = intercept[TransactionExceptionTrait](chaincodeConnection.addExam(testExam))
+          Logger.debug(s"AddInvalidExamTest: $result")
+
+          // test
+          // TODO: check specific error
+        }
+        s"indirectly $statement [$examId, $courseId, $lecturerId, $moduleId, $examType, $ects, $date, $admitUntil, $dropUntil]" in {
+          Logger.info(s"Begin test: $statement with [$examId, $courseId, $lecturerId, $moduleId, $examType, $ects, $date, $admitUntil, $dropUntil]")
+
+          // prepare data
+          val testExam = TestDataExam.customizableExam(examId, courseId, lecturerId, moduleId, examType, date, ects, admitUntil, dropUntil)
+
+          // forge approval (lecturer)
+          val result = intercept[TransactionExceptionTrait](initializeOperation(lecturerId).initiateOperation(lecturerId, "UC4.Exam", "addExam", testExam))
+          Logger.debug(s"AddInvalidExamTest: $result")
+
+          // test
+          // TODO: check specific error
+        }
+      }
     }
 
     "invoked with getExams correctly " should {
+      val allExams = chaincodeConnection.getExams(Seq().toList, Seq().toList, Seq().toList, Seq().toList, Seq().toList, "", "")
+      Logger.debug(s"All Exams: $allExams")
+
       val testData: Seq[(String, Seq[String], Seq[String], Seq[String], Seq[String], Seq[String], String, String)] = Seq(
         ("allow for getting Exams", Seq(), Seq(), Seq(), Seq(), Seq(), "", ""),
         ("allow for getting Exams2", Seq(), Seq(), Seq(), Seq(), Seq(), "", ""),
