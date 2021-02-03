58e328ce6e5c98581b5afbb86386c74532cd8b4d
Niko Bergemann <nikob@mail.uni-paderborn.de>
2021-02-03 11:39:24 +0100

    fix admission tests

--- src/test/scala/de/upb/cs/uc4/hyperledger/testUtil/TestDataAdmission.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/testUtil/TestDataAdmission.scala
@@ -1,10 +1,10 @@
 package de.upb.cs.uc4.hyperledger.testUtil
 
 object TestDataAdmission {
-  def admission1(studentId: String): String = TestDataAdmission.validAdmission(studentId, "C.1", "AdmissionModule_1", "2020-12-31T23:59:59")
-  def admission2(studentId: String): String = TestDataAdmission.validAdmission(studentId, "C.2", "AdmissionModule_3", "2020-12-31T23:59:59")
-  def admission_noAdmissionId(studentId: String): String = TestDataAdmission.validAdmissionNoAdmissionId(studentId, "C.2", "AdmissionModule_1", "2020-12-31T23:59:59")
-  def admission_noAdmissionId_WithId(studentId: String): String = TestDataAdmission.validAdmission(studentId, "C.2", "AdmissionModule_1", "2020-12-31T23:59:59")
+  def admission1(studentId: String): String = TestDataAdmission.validAdmission(studentId, "C.1", "Admission_Module_1", "2020-12-31T23:59:59")
+  def admission2(studentId: String): String = TestDataAdmission.validAdmission(studentId, "C.2", "Admission_Module_3", "2020-12-31T23:59:59")
+  def admission_noAdmissionId(studentId: String): String = TestDataAdmission.validAdmissionNoAdmissionId(studentId, "C.2", "Admission_Module_1", "2020-12-31T23:59:59")
+  def admission_noAdmissionId_WithId(studentId: String): String = TestDataAdmission.validAdmission(studentId, "C.2", "Admission_Module_1", "2020-12-31T23:59:59")
 
   def validAdmission(student: String, course: String, module: String, timestamp: String): String = {
     "{\"admissionId\":\"" + student + ":" + course + "\",\"enrollmentId\":\"" + student + "\",\"courseId\":\"" + course + "\",\"moduleId\":\"" + module + "\",\"timestamp\":\"" + timestamp + "\"}"
--- src/test/scala/de/upb/cs/uc4/hyperledger/testUtil/TestDataExam.scala
+++ src/test/scala/de/upb/cs/uc4/hyperledger/testUtil/TestDataExam.scala
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 
 object TestDataExam {
-  def validFutureExam(courseId: String, lecturerId: String, moduleId: String, examType: String = "Written Exam", ects: Int = 6): String = {
+  def validFutureExam(courseId: String, lecturerId: String, moduleId: String, examType: String, ects: Int): String = {
     val current = Calendar.getInstance()
     current.add(Calendar.MONTH, 1)
     val validAdmittableUntil = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(current.getTime)
@@ -15,17 +15,13 @@ object TestDataExam {
     validExam(courseId, lecturerId, moduleId, examType, validDate, ects, validAdmittableUntil, validDroppableUntil)
   }
 
-  def validExam(courseId: String, lecturerId: String, moduleId: String, examType: String = "Written Exam",
-      date: String = "2021-02-12T10:00:00", ects: Int = 6,
-      admittableUntil: String = "2021-01-12T23:59:59",
-      droppableUntil: String = "2021-02-05T23:59:59"): String = {
+  def validExam(courseId: String, lecturerId: String, moduleId: String, examType: String,
+      date:String, ects: Int, admittableUntil: String, droppableUntil: String): String = {
     customizableExam(s"$courseId:$moduleId:$examType:$date", courseId, lecturerId, moduleId, examType, date, ects, admittableUntil, droppableUntil)
   }
 
-  def invalidExamId(courseId: String, lecturerId: String, moduleId: String, examType: String = "Written Exam",
-      date: String = "2021-02-12T10:00:00", ects: Int = 6,
-      admittableUntil: String = "2021-01-12T23:59:59",
-      droppableUntil: String = "2021-02-05T23:59:59"): String = {
+  def invalidExamId(courseId: String, lecturerId: String, moduleId: String, examType: String,
+      date: String, ects: Int, admittableUntil: String, droppableUntil: String): String = {
     customizableExam("garbage", courseId, lecturerId, moduleId, examType, date, ects, admittableUntil, droppableUntil)
   }
 
