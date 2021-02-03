6cc4075102d3c75b729d33f462698dcc7dccef31
Niko Bergemann <nikob@mail.uni-paderborn.de>
2021-02-03 15:09:14 +0100

    Update project/Dependencies.scala
    Co-authored-by: helro <rhelmert@mail.upb.de>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,9 +6,10 @@ object Dependencies {
   private val hyperledgerGateway = "org.hyperledger.fabric" % "fabric-gateway-java" % "2.2.1"
   private val scalaTest = "org.scalatest" %% "scalatest" % "3.2.3" % Test
   private val flexmark = "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test
-  private val sl4jApi = "org.slf4j" % "slf4j-api" % "1.7.25"
-  private val sl4jLog = "ch.qos.logback" % "logback-core" % "1.2.3"
-  private val sl4jClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  private val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.25"
+  private val slf4jLog = "ch.qos.logback" % "logback-core" % "1.2.3"
+  private val slf4jClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
+
 
   // dependency groups
   val hyperledgerDependencies = Seq(hyperledgerSDK, hyperledgerGateway)
