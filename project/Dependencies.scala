92556beb15fd3bc9e8f570b97d636e1b4cc5fec2
Scala Steward <me@scala-steward.org>
2021-05-14 20:59:51 +0200

    Update scalatest to 3.2.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   // libraries
   private val hyperledgerSDK = "org.hyperledger.fabric-sdk-java" % "fabric-sdk-java" % "2.2.5"
   private val hyperledgerGateway = "org.hyperledger.fabric" % "fabric-gateway-java" % "2.2.1"
-  private val scalaTest = "org.scalatest" %% "scalatest" % "3.2.6" % Test
+  private val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9" % Test
   private val flexmark = "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test
   private val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.30"
   private val slf4jLog = "ch.qos.logback" % "logback-core" % "1.2.3"
