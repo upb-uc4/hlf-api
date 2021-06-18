afa181b3956883cc95b13a0f00196a1452562781
Scala Steward <me@scala-steward.org>
2021-06-18 08:47:26 +0200

    Update slf4j-api to 1.7.31

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   private val hyperledgerGateway = "org.hyperledger.fabric" % "fabric-gateway-java" % "2.2.1"
   private val scalaTest = "org.scalatest" %% "scalatest" % "3.2.6" % Test
   private val flexmark = "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test
-  private val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.30"
+  private val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.31"
   private val slf4jLog = "ch.qos.logback" % "logback-core" % "1.2.3"
   private val slf4jClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
 
